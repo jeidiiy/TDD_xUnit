4c24b06a70296db469a91218b5a4fb1f010bb691
jeidiiy <wldyd2356@gmail.com>
2022-05-27 17:19:40 +0900

    부트스트랩 테스트

--- Main.py
+++ Main.py
@@ -4,3 +4,15 @@
 # TODO: 테스트 메서드가 실패하더라도 tearDown 호출하기
 # TODO: 여러 개의 테스트 실행하기
 # TODO: 수집된 결과를 출력하기
+
+class WasRun:
+    def __init__(self, name):
+        self.wasRun = None
+
+    def testMethod(self):
+        self.wasRun = 1
+
+test = WasRun('testMethod')
+print(test.wasRun)
+test.testMethod()
+print(test.wasRun)

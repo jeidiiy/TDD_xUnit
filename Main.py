3975b07c463507607c2768470b6688dfa6b64591
jeidiiy <wldyd2356@gmail.com>
2022-05-27 18:30:19 +0900

    테스트 실행 결과를 기록하는 객체 반환 2단계: 변수화)
    수집한 결과를 출력하기 TODO 완료

--- Main.py
+++ Main.py
@@ -1,10 +1,13 @@
 # TODO: 테스트 메서드가 실패하더라도 tearDown 호출하기
 # TODO: 여러 개의 테스트 실행하기
-# TODO: 수집된 결과를 출력하기
 
 class TestResult:
+    def __init__(self):
+        self.runCount = 0
+    def testStarted(self):
+        self.runCount += 1
     def summary(self):
-        return "1 run, 0 failed"
+        return "%d run, 0 failed" % self.runCount
 
 class TestCase:
     def __init__(self, name):
@@ -14,11 +17,13 @@ class TestCase:
         pass
 
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
-        return TestResult()
+        return result
 
     def tearDown(self):
         pass
@@ -35,7 +40,15 @@ class WasRun(TestCase):
     def tearDown(self):
         self.log = self.log + "tearDown "
 
+    def testBrokenMethod(self):
+        raise Exception
+
 class TestCaseTest(TestCase):
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
+
     def testResult(self):
         test = WasRun("testMethod")
         result = test.run()
@@ -48,3 +61,4 @@ class TestCaseTest(TestCase):
 
 TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testResult").run()
+TestCaseTest("testFailedResult").run()

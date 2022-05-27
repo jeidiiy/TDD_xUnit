f3d189e2131f20f114e975cdcf3470b6c46f2cc7
jeidiiy <wldyd2356@gmail.com>
2022-05-27 17:42:29 +0900

    먼저 setup 호출하기 TODO 완료

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-# TODO: 먼저 setUp 호출하기
 # TODO: 나중에 tearDown 호출하기
 # TODO: 테스트 메서드가 실패하더라도 tearDown 호출하기
 # TODO: 여러 개의 테스트 실행하기
@@ -8,23 +7,36 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
         TestCase.__init__(self, name)
+
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetup = 1
 
     def testMethod(self):
         self.wasRun = 1
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
+    def testSetup(self):
+        self.test.run()
+        assert(self.test.wasSetup)
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetup").run()

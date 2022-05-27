eac47d9baa7c2a41a8e7fc5ff80a8849105225e3
jeidiiy <wldyd2356@gmail.com>
2022-05-27 18:06:46 +0900

    나중에 tearDown 호출하기 TODO 완료

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-# TODO: 나중에 tearDown 호출하기
 # TODO: 테스트 메서드가 실패하더라도 tearDown 호출하기
 # TODO: 여러 개의 테스트 실행하기
 # TODO: 수집된 결과를 출력하기
@@ -14,11 +13,12 @@ class TestCase:
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
+
+    def tearDown(self):
+        pass
 
 class WasRun(TestCase):
-    def __init__(self, name):
-        TestCase.__init__(self, name)
-
     def setUp(self):
         self.wasRun = None
         self.log = "setUp "
@@ -27,10 +27,13 @@ class WasRun(TestCase):
         self.wasRun = 1
         self.log = self.log + "testMethod "
 
+    def tearDown(self):
+        self.log = self.log + "tearDown "
+
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         self.test = WasRun("testMethod")
         self.test.run()
-        assert("setUp testMethod " == self.test.log)
+        assert("setUp testMethod tearDown " == self.test.log)
 
 TestCaseTest("testTemplateMethod").run()
